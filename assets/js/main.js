57ded0fef65ac83fb6f9577973ba39d4bb62c445
artanito <artani799@gmail.com>
2022-09-09 16:45:09 +0200

    First Commint

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const navigation = document.getElementById("nav");
+const menu = document.getElementById("hamburger");
+
+menu.addEventListener("click", () => {
+  navigation.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+$(document).ready(function () {
+  //Maruf al Bashir Reza
+  $(window).on('load', function () { // makes sure the whole site is loaded 
+    $('#status').fadeOut(); // will first fade out the loading animation 
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(550).css({ 'overflow': 'visible' });
+  });
+});
---
+++ send-mail.php
@@ -0,0 +1,32 @@
+<?php
+
+if (isset($_POST['email'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $phone = $_POST['phone'];
+  $message = $_POST['message'];
+  $res['status'] = false;
+  $res['message'] = "";
+
+  $to = 'artani799@gmail.com';
+  $subject = "Contact from Message";
+  $messageMl = '<p>Name : '.$name.'</p><br> <p>Email : '.$email.'</p><br> <p>Phone : '.$phone.'</p><br><p>Message : '.$message.'</p><br>';
+
+  // Always set content-type when sending HTML email
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+  // More headers
+  $headers .= 'From: <web@thirdpillarrecruiting.com>' . "\r\n";
+
+  $sendMail = mail($to, $subject, $messageMl, $headers);
+  if ($sendMail) {
+    $res['status'] = true;
+    $res['message'] = "Mail Send Success";
+  } else {
+    $res['status'] = false;
+    $res['message'] = "Problem to sending mail";
+  }
+
+  echo json_encode($res);
+}
